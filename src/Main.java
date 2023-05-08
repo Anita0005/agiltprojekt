33339a7da325bf1ebd112e10a71989f53e53a064
zakariahkr <132570409+zakariahkr@users.noreply.github.com>
2023-05-08 13:09:43 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args)  {
         MyFrame frame = new MyFrame();
+        import java.sql.Connection;
+        import java.sql.DriverManager;
+        import java.sql.Statement;
+        import java.sql.ResultSet;
+        
+        public static void main(String[] args)  {
+        
+        try {
+            
+            Connection connection =
+        DriverManager.getConnection("jdbc:mysql://localhost:3 306/demo","root", "Root123");
+        
+        Statement statement = connection.createStatement();
+            
+        System.out.printIn(resultSet.getString("firstname"));
+        
+        }
+         
+        } catch (Exceprion e) {
+            e.printStackTrace();
+        }
     }
 }
