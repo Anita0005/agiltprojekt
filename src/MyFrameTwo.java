5fda1690d3d819ec616bdcba6585059bae1dbcd5
zakariahkr <132570409+zakariahkr@users.noreply.github.com>
2023-05-15 15:01:39 +0200

    Create MyFrameTwo.java

---
+++ src/MyFrameTwo.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionListener;
+
+public class MyFrameTwo extends JFrame implements ActionListener{
+
+
+    MyFrameTwo() {
+        this.setSize(600, 700);
+        this.setTitle("Platform");
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        //Frame created.
+    }
+    @Override
+	public void actionPerformed(ActionEvent e){
+    }
+}
