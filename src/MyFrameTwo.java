1512733bef20bb29fe2472db95b347dfddb1f659
Anita0005 <anita.vujica0005@stud.hkr.se>
2023-05-20 13:58:16 +0200

    hh

--- src/MyFrameTwo.java
+++ src/MyFrameTwo.java
@@ -6,7 +6,10 @@ import java.awt.event.ActionListener;
 
 public class MyFrameTwo extends JFrame implements ActionListener{
 
-    JLabel projectname2;
+    JLabel projectname2, projectsloga2, registration_successful, question, image;
+    JPanel register_message;
+    JButton next,exit;
+    ImageIcon icon;
 
     MyFrameTwo() {
         this.setSize(600, 700);
@@ -25,12 +28,47 @@ public class MyFrameTwo extends JFrame implements ActionListener{
         projectname2.setHorizontalAlignment(JLabel.CENTER);
         projectname2.setBounds(120, 50, 350, 550);
 
+        projectsloga2 = new JLabel();
+        projectsloga2.setText("STÄRK DIN HJÄRNA, STÄRK DITT LIV");
+        projectsloga2.setFont(new Font("Times New Roman", Font.BOLD, 10));
+        projectsloga2.setVerticalAlignment(JLabel.TOP);
+        projectsloga2.setHorizontalAlignment(JLabel.CENTER);
+        projectsloga2.setBounds(120, 85, 350, 550);
+
+        register_message = new JPanel();
+        register_message.setLayout(null);
+        register_message.setBackground(new Color(230, 243, 250));
+        register_message.setBounds(120,200,350,370);
+
+        registration_successful = new JLabel("Registration successful!");
+        registration_successful.setBounds(66,30,350,40);
+        registration_successful.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        register_message.add(registration_successful);
+
+        question = new JLabel();
+        question.setText("Do you wish to continue?");
+        question.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+        question.setBounds(85, 200, 370, 40);
+        register_message.add(question);
+
+        // icon = new ImageIcon("icon.png");
+        // image = new JLabel(icon);
+        // image.setBounds(50,50,50,50);
+        // register_message.add(image);
+
+        next = new JButton("Next");
+        next.setBounds(133,250,90,30);
+        register_message.add(next);
+
+        exit = new JButton("Exit");
+        exit.setBounds(133,300,90,30);
+        register_message.add(exit);
 
 
+        this.add(register_message);
         this.add(projectname2);
+        this.add(projectsloga2);
         this.setVisible(true);
-
-
 
 
     }
